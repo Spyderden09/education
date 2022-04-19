8f334cab6e0009dbcc621c73b8867e486a6e9983
Spyderden09 <91795449+Spyderden09@users.noreply.github.com>
2022-04-19 21:38:51 +0300

    Changes

--- modules/game/load.php
+++ modules/game/load.php
@@ -1,1 +1,8 @@
 <?php
+$user_hp = q("SELECT * FROM `game` WHERE `user_id` = ".$_SESSION['user']['id']);
+if ($user_hp->num_rows) {
+    $_SESSION["user_data"] = $user_hp->fetch_assoc();
+    echo "<script>myLoad();</script>";
+}else{
+    echo "<script>hideShow('st_block');</script>";
+}
--- modules/game/main.php
+++ modules/game/main.php
@@ -1,15 +1,8 @@
 <?php
-$user_hp = q("SELECT * FROM `game` WHERE `user_id` = ".$_SESSION['user']['id']);
-if ($user_hp->num_rows) {
-    $user_hp_arr = $user_hp->fetch_assoc();
-    echo "<script>myLoad()</script>";
-}else{
-    echo "<script>myRestart()</script>";
-}
-if (isset($_SESSION['user'])){
+if  (isset($_SESSION['user'])){
     if (!isset($_SESSION['vis'])){
         $_SESSION['vis'] = "<h1>Пожалуйста, введите число от 1-ого до 3-ёх</h1>";
     }
 }else{
     header("Location: /");
-  }
+}
